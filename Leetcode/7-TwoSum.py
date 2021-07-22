6899c5ad9cc60a5303bfdc674a672fd79bcc761a
Akshay <moreakshay725@gmail.com>
2021-07-22 13:33:18 +0530

    changes

--- Leetcode/7-TwoSum.py
+++ Leetcode/7-TwoSum.py
@@ -1,12 +1,19 @@
-class Solution(object):
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+
+class Solution():
     def twoSum(self, nums, target):
-        for i in range(0 , len(nums) - 1):
-            for j in range(1, len(nums)):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
                 complement = target - nums[i]
                 if(nums[j] == complement):
-                    print(i, j)
+                    return [i, j]
 
 obj = Solution()
-nums = [2,7,11,15]
-target = 9
-obj.twoSum(nums, target)
+nums = [2,5,5,15]
+target = 10
+print(obj.twoSum(nums, target))
